8661449e3d103c6817a9939ecbecdbc51fd169fa
angryduck <nogo22@naver.com>
2021-01-08 04:20:00 +0900

    js and css func complete !

--- index.php
+++ index.php
@@ -27,60 +27,31 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <link rel="stylesheet" href="index.css">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mkdoc</title>
-        <script>
-
-        function dayHandler(self){
-            var target = document.querySelector('body');
-            if (self.value === 'dark') {
-                target.style.backgroundColor = 'black';
-                target.style.color = 'white';
-                self.value = 'light';
-                
-                var list = document.querySelectorAll('a');
-                for (var i = 0; i < list.length; i++) {
-                    list[i].style.color = 'antiquewhite'
-                }
-            } else {
-                target.style.backgroundColor = 'antiquewhite';
-                target.style.color = 'black';
-                self.value = 'dark';
-                
-                var list = document.querySelectorAll('a');
-                for (var i = 0; i < list.length; i++) {
-                    list[i].style.color = 'black'
-                }
-            }
-        }
-    </script>
 </head>
 <body>
     <div id="topbar">
-
         <h1><a href="index.php">web22</a></h1>
     </div>
-
     <div id="main">
-    <div id="listbar">
-
-    <ol>
-        <?php printList(); ?>
-    </ol>
+        <div id="listbar">
+            <ol>
+                <?php printList(); ?>
+            </ol>
         </div>
-
         <div id="content">
-    <h2>
-        <?php printTitle(); ?>  
-    </h2>
-    <?php printContent(); ?>
+            <h2>
+                <?php printTitle(); ?>  
+            </h2>
+            <?php printContent(); ?>
         </div>
     </div>
     <div id="funcbar">
         <input type="button" value="dark" onclick="dayHandler(this)">
     </div>
+    <script src="index.js"></script>
 </html>
